86aed1a8c625deb63c2d5334e60b48ab2ec13584
Fideligo <mrafighalibf@gmail.com>
2026-04-02 10:14:55 +0700

    refactor: migrate gateway to use gin framework for routing and JSON binding

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type DocumentRequest struct {
@@ -13,38 +13,37 @@ type DocumentRequest struct {
 }
 
 func main() {
+
+	router := gin.Default()
 	
 	// Health Check (GET)
-	http.HandleFunc("/api/health", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "SecondBrain Gateaway is running smoothly!")
+
+	router.GET("/api/health", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H {
+			"message": "SecondBrain Gateaway is running smoothly!",
+		})
 	})
 
 	// Menerima data JSON (POST)
-	http.HandleFunc("/api/upload", func(writer http.ResponseWriter, request *http.Request) {
 
-		if request.Method != http.MethodPost {
-			http.Error(writer, "Only accepts POST method", http.StatusMethodNotAllowed)
-			return
-		}
-
+	router.POST("/api/upload", func(context *gin.Context) {
 		var docReq DocumentRequest
 
-		err := json.NewDecoder(request.Body).Decode(&docReq)
-		if err != nil {
-			http.Error(writer, "Invalid JSON Format", http.StatusBadRequest)
+		if err := context.ShouldBindJSON(&docReq); err != nil {
+			context.JSON(http.StatusBadRequest, gin.H {
+				"error": "Invalid JSON Format or wrong data type",
+			})
 			return
 		}
 
 		fmt.Printf("New Document!\nFile Name: %s\nWriter: %s\n", docReq.FileName, docReq.Author)
 
-		writer.WriteHeader(http.StatusAccepted) // Memberikan kode status HTTP 202 Accepted
-		fmt.Fprintf(writer, "The Document %s from %s is accepted and being processed.", docReq.FileName, docReq.Author)
+		context.JSON(http.StatusAccepted, gin.H{
+			"message": fmt.Sprintf("The Document %s from %s is accepted", docReq.FileName, docReq.Author),
+			"status":  "Processing",
+		})
 	})
 
-	port := ":8080"
-	fmt.Printf("Gateaway starting on port%s...\n", port)
-
-	if err := http.ListenAndServe(port, nil); err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
+	fmt.Println("🚀 Gateway starting on port :8080...")
+	router.Run(":8080")
 }
