1dfebedb6913fe6d43858877abc296af30a2d07e
Fideligo <mrafighalibf@gmail.com>
2026-04-02 09:23:02 +0700

    feat: add endpoint for post request that accepts json

--- main.go
+++ main.go
@@ -19,6 +19,28 @@ func main() {
 		fmt.Fprintf(writer, "SecondBrain Gateaway is running smoothly!")
 	})
 
+	// Menerima data JSON (POST)
+	http.HandleFunc("/api/upload", func(writer http.ResponseWriter, request *http.Request) {
+
+		if request.Method != http.MethodPost {
+			http.Error(writer, "Only accepts POST method", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var docReq DocumentRequest
+
+		err := json.NewDecoder(request.Body).Decode(&docReq)
+		if err != nil {
+			http.Error(writer, "Invalid JSON Format", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Printf("New Document!\nFile Name: %s\nWriter: %s\n", docReq.FileName, docReq.Author)
+
+		writer.WriteHeader(http.StatusAccepted) // Memberikan kode status HTTP 202 Accepted
+		fmt.Fprintf(writer, "The Document %s from %s is accepted and being processed.", docReq.FileName, docReq.Author)
+	})
+
 	port := ":8080"
 	fmt.Printf("Gateaway starting on port%s...\n", port)
 
